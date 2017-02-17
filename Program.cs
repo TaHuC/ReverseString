1f76d679382e89823fa0e06f6ad30599ed5a0295
TaHuC <TaHuC@hotmail.com>
2017-02-17 23:33:12 +0200

    Complate Reverse String

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReverseString
+{
+	public class MainClass
+	{
+		public static void Main()
+		{
+			var getString = Console.ReadLine();
+
+			char[] reverse = getString.ToCharArray();
+			Array.Reverse(reverse);
+
+			Console.WriteLine(new string(reverse));
+		}
+	}
+}
